44f823adf0506997ec810940a5d6320f987ebf58
Kyle-Muloki <kylemuloki@gmai.com>
2024-06-06 20:59:07 +0300

    css

--- networking_in_heels/urls.py
+++ networking_in_heels/urls.py
@@ -17,14 +17,16 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from tickets import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tickets/', include('tickets.urls')),
-    path('', views.purchase_ticket, name='home'),  # Home view for simplicity
+    path('tickets/', include('tickets.urls')),  # Include the tickets app URLs
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
